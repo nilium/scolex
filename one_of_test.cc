7752dad346f2de88496397725da1052f9ee8c587
Noel Cower <ncower@gmail.com>
2014-09-09 02:53:52 -0700

    Add test code for one_of.hh

---
+++ one_of_test.cc
@@ -0,0 +1,273 @@
+#include <iostream>
+#include "one_of.hh"
+
+int main(int argc, char const *argv[])
+{
+  std::cout
+    << R"raw(0 == one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          0 == one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(1 == one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          1 == one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(2 == one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          2 == one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(3 == one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          3 == one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(4 == one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          4 == one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(5 == one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          5 == one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(6 == one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          6 == one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw('\0' == one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          '\0' == one_of(' ', '\t', '\r', '\n')
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw('\n' == one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          '\n' == one_of(' ', '\t', '\r', '\n')
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw('\r' == one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          '\r' == one_of(' ', '\t', '\r', '\n')
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw('\t' == one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          '\t' == one_of(' ', '\t', '\r', '\n')
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw( 'f' == one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+           'f' == one_of(' ', '\t', '\r', '\n')
+      // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw( ' ' == one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+           ' ' == one_of(' ', '\t', '\r', '\n')
+      // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(0 != one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          0 != one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(1 != one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          1 != one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(2 != one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          2 != one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(3 != one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          3 != one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(5 < one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          5 < one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+  std::cout
+    << R"raw(5 <= one_of(1, 2, 3, 4, 5)              => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          5 <= one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(5 != one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          5 != one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw(6 != one_of(1, 2, 3, 4, 5)               => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          6 != one_of(1, 2, 3, 4, 5)
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw('\0' != one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          '\0' != one_of(' ', '\t', '\r', '\n')
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw('\n' != one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          '\n' != one_of(' ', '\t', '\r', '\n')
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw('\r' != one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          '\r' != one_of(' ', '\t', '\r', '\n')
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw('\t' != one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+          '\t' != one_of(' ', '\t', '\r', '\n')
+    // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw( 'f' != one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+           'f' != one_of(' ', '\t', '\r', '\n')
+      // END
+    ) << std::endl;
+
+
+
+  std::cout
+    << R"raw( ' ' != one_of(' ', '\t', '\r', '\n')    => )raw"
+    << std::boolalpha << (
+    // BEGIN
+           ' ' != one_of(' ', '\t', '\r', '\n')
+      // END
+    ) << std::endl;
+
+  return 0;
+}
